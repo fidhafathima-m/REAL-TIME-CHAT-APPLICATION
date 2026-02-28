159b07e9fd101c3608da26ebf51e50e03603d2ac
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2026-02-28 12:14:24 +0530

    server setted up using express, socket.io and http

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require("cors")
+const socketIO = require("socket.io")
+const http = require("http")
+const app = express()
+const port = 3000
+
+app.use(cors({
+    origin: '*'
+}))
+const server = http.createServer(app)
+const io = socketIO(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message);       // broadcast message to all connected clients
+    })
+    socket.on("disconnect", (message) => {
+        console.log("User disconnected")
+    })
+})
+
+server.listen(port, () => console.log(`Server running on port ${port}!`))
